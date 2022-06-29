2f0f99b4839f4966fdc0ff077c18625aa208b8b6
Joshua Amaju <princejosh0007@gmail.com>
2022-06-30 00:49:56 +0100

    chore: resolve module alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,29 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'module:react-native-dotenv',
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: [
+          '.ios.{js|ts|tsx}',
+          '.android.{js|ts|tsx}',
+          '.js',
+          '.ts',
+          '.tsx',
+          '.json',
+        ],
+        alias: {
+          '@global': ['./global/'],
+          '@screens': ['./screens/'],
+          '@exports': ['./exports/'],
+          '@utils': ['./global/utils/'],
+          '@assets': ['./global/assets/'],
+          '@stores': ['./global/stores/'],
+          '@components': ['./global/components/'],
+        },
+      },
+    ],
+  ],
 };
