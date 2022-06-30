94d23f40218b245278e19613b66ae90258d6427c
Joshua Amaju <princejosh0007@gmail.com>
2022-06-30 01:10:41 +0100

    chore: export svg icons

---
+++ exports/icons.ts
@@ -0,0 +1,8 @@
+export {default as Exit} from '@assets/icons/exit.svg';
+export {default as Wallet} from '@assets/icons/wallet.svg';
+export {default as EyeOff} from '@assets/icons/eye.off.svg';
+export {default as MoneySend} from '@assets/icons/money.send.svg';
+export {default as ReceiptText} from '@assets/icons/receipt.text.svg';
+export {default as ChevronDown} from '@assets/icons/chevron.down.svg';
+export {default as ChevronRight} from '@assets/icons/chevron.right.svg';
+export {default as ChevronLeftCircle} from '@assets/icons/chevron.left.circle.svg';
