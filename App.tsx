6c72328ddf2e12b2c788f84d808ee1b0bc4d91aa
Joshua Amaju <princejosh0007@gmail.com>
2022-06-30 01:10:59 +0100

    chore: setup navigator

--- App.tsx
+++ App.tsx
@@ -1,8 +1,42 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import FlashMessage from 'react-native-flash-message';
+
+import BackButton from '@components/backbutton';
+
+// screens
+import Root from '@screens/root';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return <SafeAreaView></SafeAreaView>;
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShadowVisible: false,
+            headerTitleAlign: 'center',
+            contentStyle: {backgroundColor: '#fff'},
+            headerLeft: props => props.canGoBack && <BackButton />,
+          }}>
+          <Stack.Screen
+            name="Root"
+            component={Root}
+            options={{
+              headerShown: false,
+              // contentStyle: {backgroundColor: '#fff'},
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+      <FlashMessage />
+    </>
+  );
 };
 
 export default App;
